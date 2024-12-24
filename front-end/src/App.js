87db88b792f60b38fa6a6adac3e2e7e1f867c0fd
Shiva Verma <shivaverma30july@gmail.com>
2024-12-25 00:44:39 +0530

    components created

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import About from "./components/About"
+import Events from "./components/Events"
+import Testimonial from "./components/Testimonial"
+import Gallery from "./components/Gallery"
+import News from "./components/News"
 function App() {
   return (
     <div className="App">
-      
+      <About/>
+      <Events/>
+      <Testimonial/>
+      <Gallery/>
+      <News/>
     </div>
   );
 }
